0b8b3bd295f77f9f92214390542c6af78531e7e2
Jonathon Joyner <jonathonandon@gmail.com>
2020-09-06 16:44:07 -0500

    added color message and h1

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  $('#app').html('<h1>Webpack 5</h1>');
+  console.warn('Webpack 5');
 };
 
 init();
